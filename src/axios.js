7555a022166449ede6894569fae4ddb469e38030
Shrey-zionn <shreyansh@zionn.trade>
2022-12-12 19:56:16 +0530

    payDone logic

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const API = axios.create({
-    baseURL: 'https://fmbackend.onrender.com/'
+    baseURL: 'http://localhost:8080/'
 })
 
 API.interceptors.request.use((req) => {
